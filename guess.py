eba8ce8d3b512c19df2998d9b0c624743b9ddfc6
Rumiah Kessel <rukessel@gmail.com>
2022-11-15 13:48:46 -0800

    guess.py

---
+++ guess.py
@@ -0,0 +1,27 @@
+import networkx as nx
+import numpy as np
+from starter import *
+import random
+import math
+
+def solver(G: nx.graph):
+    mu = G.size(weight='weight') / G.size()
+    k = math.floor(4 * np.log(np.sqrt(mu) * G.number_of_nodes() / 10))
+    nodes = []
+    for v in G.nodes:
+        nodes += [v]
+    random.shuffle(nodes)
+    for i in range(len(nodes)):
+        G.nodes[nodes[i]]['team'] = i % k
+    
+    return G
+
+guess = read_input('inputs/example.in')
+guess = solver(guess)
+
+example = read_input('inputs/example.in')
+example = read_output(example, 'outputs/example.out')
+
+print(score(guess), score(example))
+
+visualize(guess)
