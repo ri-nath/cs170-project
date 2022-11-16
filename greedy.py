d1b3d8db9746641afff158be9e00a22de6e94035
Rishi Nath <rishi.nath@berkeley.edu>
2022-11-16 00:45:36 -0800

    refactor greedy

--- greedy.py
+++ greedy.py
@@ -4,88 +4,35 @@ from starter import *
 import math
 import random
 
+# given G and a subset S, returns a fn(v) which sums weights from v to S 
+def sum_weights_to_subset(G, S):
+    def from_vertex(v):
+        edges = [G[v][u]['weight'] for u in G.neighbors(v) if u in S]
+        return sum(edges)
+    return from_vertex
 
+def solver(G: nx.graph, sources: list[int] = [0, 10, 20, 30, 40]):    
+    k = len(sources)
+    teams = [[s] for s in sources]
 
-def solver(Gold: nx.graph):
-    G = Gold.copy()
-    edges = G.edges(data='weight')
-    mu = G.size(weight='weight') / G.size()
-    # k = math.floor(4 * np.log(np.sqrt(mu) * G.number_of_nodes() / 10))
-    k = 5
-    m = float('inf')
-    bk = 0
-    # for k in range(2,G.number_of_nodes()):
-    for i in range(G.number_of_nodes()):
-        G.nodes[i]['visited'] = 0
-    print(k)
-    prev = [None for _ in range(k)]
-    teams = [[] for _ in range(k)]
-    u, v, _ = sorted(edges, reverse=True, key=lambda x: x[2])[0]
-    # team = 0
-    # G.nodes[u]['team'] = team
-    # G.nodes[u]['visited'] = 1
-    # prev[team] = u
-    # teams[team] += [u]
-    
-    # team += 1
-    # for i in range(1, k):
-    #     pre = (team-1)%k
-    #     print("pre: ", pre)
-    #     print("prev: ", prev)
-    #     next = max([u for u in G.neighbors(prev[pre]) if G.nodes[u]['visited'] == 0], key=lambda x: G[prev[pre]][x]['weight'])
-    #     print("weight: ", G[prev[pre]][next]['weight'])
-    #     G.nodes[next]['visited'] = 1
-    #     G.nodes[next]['team'] = team % k
-    #     prev[team%k] = next
-    #     teams[team%k] += [next]
-    #     team += 1
     team = 0
-    prev = [0, 10, 20, 30, 40]
-    teams = [[0], [10], [20], [30], [40]]
-    for u in prev:
-        G.nodes[u]['team'] = team
-        G.nodes[u]['visited'] = 1
-        team += 1
-    print("--------------------------------------------")
-    while team < G.number_of_nodes():
-        curr = (team)%k
-        print("curr: ", curr)
-        print("prev: ", prev)
-        print("teams: ", teams)
-        next = min([n for n in G.nodes() if G.nodes[n]['visited'] == 0], key=lambda x: sum([G[u][x]['weight'] for u in G.neighbors(x) if u in [u for u in teams[curr]]]))
-        G.nodes[next]['visited'] = 1
-        G.nodes[next]['team'] = team % k
-        prev[team%k] = next
-        teams[team%k] += [next]
-        team += 1
+    for u in G.nodes:
+        if u in sources:
+            G.nodes[u]['team'] = team + 1
+            G.nodes[u]['visited'] = True
+            team += 1
+        else:
+            G.nodes[u]['visited'] = False
+    
+    for i in range(k, G.number_of_nodes()):
+        team = i % k
+        not_visited = filter(lambda v: not G.nodes[v]['visited'], G.nodes)
+        next = min(not_visited, key=sum_weights_to_subset(G, teams[team]))
+        G.nodes[next]['team'] = team + 1
+        G.nodes[next]['visited'] = True
+        teams[team].append(next)
 
-    
-    
-    
-    # for u, v, _ in sorted(edges, reverse=False, key=lambda x: x[2]):
-    #     if 'team' in G.nodes[u] and 'team' in G.nodes[v]:
-    #         pass
-    #     elif 'team' in G.nodes[u]:
-    #         G.nodes[v]['team'] = team
-    #         team = team + 1 % k
-    #     elif 'team' in G.nodes[v]:
-    #         G.nodes[u]['team'] = team
-    #         team = team + 1 % k
-    #     else:
-    #         G.nodes[u]['team'] = team
-    #         G.nodes[v]['team'] = team + 1 % k
-    #         team = team + 2 % k
-    s = score(G)
-    if(s < m):
-        m = s
-        bk = k
-        Gout = G.copy()
-    print("MIN__________________: ", m)
-
-    # Band-aid patch, teams should be one-indexed
-    for v in Gout.nodes:
-        Gout.nodes[v]['team'] = Gout.nodes[v]['team'] + 1
-    return Gout
+    return G
 
 greedy = read_input('inputs/example.in')
 greedy = solver(greedy)
@@ -93,6 +40,6 @@ greedy = solver(greedy)
 example = read_input('inputs/example.in')
 example = read_output(example, 'outputs/example.out')
 
-print(score(greedy, separated=True), score(example, separated=True))
+print(score(greedy), score(example))
 
-# visualize(greedy)
+visualize(greedy)
